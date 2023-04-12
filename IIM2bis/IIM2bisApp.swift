a6f74a5b147443f29a6826982d35a636ae6d29d0
djinodissingar <dissingardjino@icloud.com>
2023-04-12 14:58:36 +0200

    Initial Commit

---
+++ IIM2bis/IIM2bisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIM2bisApp.swift
+//  IIM2bis
+//
+//  Created by Djino Dissingar on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct IIM2bisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IIM2bisUITests/IIM2bisUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IIM2bisUITestsLaunchTests.swift
+//  IIM2bisUITests
+//
+//  Created by Djino Dissingar on 12/04/2023.
+//
+
+import XCTest
+
+final class IIM2bisUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
