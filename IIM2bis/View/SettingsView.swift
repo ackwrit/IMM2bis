b51a46f1d6dc39727246c12d8e4338c477b46de2
djinodissingar <dissingardjino@icloud.com>
2023-04-14 17:53:54 +0200

    mise à jour 5

--- IIM2bis/View/SettingsView.swift
+++ IIM2bis/View/SettingsView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var prenom = ""
+    @State var nom = ""
     @StateObject var userVm : UserViewModel
     var body: some View {
-        Text(userVm.user?.mail ?? "")
+        VStack{
+            Spacer()
+            Image("tomb")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 400,height: 200)
+            Text(userVm.user?.fullName ?? "")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+            List {
+                Section {
+                    HStack {
+                        TextField(userVm.user?.name ?? "", text: $prenom)
+                        
+                        Button {
+                            //metter les informations
+                            if(prenom != ""){
+                                var datas:[String:Any] = ["PRENOM": prenom]
+                                FirebaseManager().updateUserFirebase(uid: userVm.user?.id ?? "", datas: datas)
+                                prenom = ""
+                            }
+                        } label: {
+                            Image(systemName: "plus")
+                            
+                        }
+                        
+                        
+                    }
+                    
+                    
+                    HStack {
+                        TextField(userVm.user?.lastName ?? "", text: $nom)
+                        
+                        Button {
+                            //metter les informations
+                            if(nom != ""){
+                                var datas:[String:Any] = ["NOM": nom]
+                                FirebaseManager().updateUserFirebase(uid: userVm.user?.id ?? "", datas: datas)
+                                nom = ""
+                            }
+                        } label: {
+                            Image(systemName: "plus")
+                            
+                        }
+                        
+                        
+                    }
+                   
+
+                }
+            }
+            
+        }
     }
 }
 
